3b2452d0bb30d9f054834d1c98360aa3c58ccc1e
christux <Administrator@S4A.formation.local>
2017-01-12 13:55:14 +0100

    update check validity function

--- BmiCalculator/src/application/Main.java
+++ BmiCalculator/src/application/Main.java
@@ -29,55 +29,29 @@ public class Main extends Application {
 	private TextField tflSize;
 	private TextField tflWeight;
 	
-	private boolean checkValidity() {
+	private boolean matchWithRegex(TextField tf, String reg) {
 		
-		boolean result=true;
 		String error = "-fx-text-box-border: red;";
 		String noerror = "-fx-text-box-border: green;";
-    	
-		if(tflFirstName.getText().isEmpty()) {
-			tflFirstName.setStyle(error);
-			result=false;
-		} else {
-			tflFirstName.setStyle(noerror);
+		
+		if(!tf.getText().matches(reg)) {
+			tf.setStyle(error);
+			return false;
 		}
 		
-		if(tflLastName.getText().isEmpty()) {
-			tflLastName.setStyle(error);
-			result=false;
-		} else {
-			tflLastName.setStyle(noerror);
-		}
+		tf.setStyle(noerror);
+		return true;
+	}
 		
-		if(tflSize.getText().isEmpty()) {
-			tflSize.setStyle(error);
-			result=false;
+	private boolean checkValidity() {
 			
-		} else {
-			if(!tflSize.getText().matches("^[0-9]+$")) {
-				tflSize.setStyle(error);
-				result=false;
-			}
-			else {
-				tflSize.setStyle(noerror);
-			}
-		}
-		
-		if(tflWeight.getText().isEmpty()) {
-			tflWeight.setStyle(error);
-			result=false;
-		} else {
-			if(!tflWeight.getText().matches("^[0-9]+$")) {
-				tflWeight.setStyle(error);
-				result=false;
-			}else {
-				tflWeight.setStyle(noerror);
-			}
-		}
-		
-		
-    	return result;
-    }
+			if(!matchWithRegex(tflFirstName,"^[a-z A-Z]+$")) return false;
+			if(!matchWithRegex(tflLastName,"^[a-z A-Z]+$")) return false;
+			if(!matchWithRegex(tflSize,"^[0-9]+$")) return false;
+			if(!matchWithRegex(tflWeight,"^[0-9]+$")) return false;
+			
+	    	return true;
+	    }
 	
 	@Override
 	public void start(Stage primaryStage) {
