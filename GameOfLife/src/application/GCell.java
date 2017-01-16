7be76d110862de4f4c26c440d3906710a3e66893
christux <Administrator@10.138.100.10>
2017-01-16 16:51:12 +0100

    add GCell

---
+++ GameOfLife/src/application/GCell.java
@@ -0,0 +1,18 @@
+package application;
+
+public class GCell {
+	
+	int x,y;
+	int size;
+
+	public GCell(int x, int y, int size) {
+		this.x=x;
+		this.y=y;
+		this.size=size;
+	}
+	
+	public void update(boolean isAlive) {
+		
+	}
+
+}
