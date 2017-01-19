a1ff5a763e6917ada883d78006249ba02b1f0836
christux <Administrator@S4A.formation.local>
2017-01-19 10:47:59 +0100

    improve tests

--- SimpleList/tests/tests/TestSimpleList.java
+++ SimpleList/tests/tests/TestSimpleList.java
@@ -80,33 +80,33 @@ public class TestSimpleList {
 	
 	@Test
 	public void testDeleteObject() {
-		SimpleList<Integer> list = new SimpleList<Integer>();
-		Integer a=new Integer(0);
-		Integer b=new Integer(-4);
+		SimpleList<Double> list = new SimpleList<Double>();
+		Double a=new Double(0.15);
+		Double b=new Double(-4.4);
 		list.add(a);
-		list.add(new Integer(2));
+		list.add(new Double(2.0));
 		list.add(b);
-		list.add(new Integer(7));
-		list.add(new Integer(-2));
+		list.add(new Double(7.2));
+		list.add(new Double(-2.9));
 		
 		assertEquals(5, list.count());
 		
 		list.delete(b);
 		
 		assertEquals(4, list.count());
-		assertEquals(new Integer(7), list.get(2));
+		assertEquals(new Double(7.2), list.get(2));
 	}
 	
 	@Test
 	public void testFindObject() {
-		SimpleList<Integer> list = new SimpleList<Integer>();
-		Integer a=new Integer(0);
-		Integer b=new Integer(-4);
+		SimpleList<Float> list = new SimpleList<Float>();
+		Float a=new Float(0);
+		Float b=new Float(-4);
 		list.add(a);
-		list.add(new Integer(2));
+		list.add(new Float(2));
 		list.add(b);
-		list.add(new Integer(7));
-		list.add(new Integer(-2));
+		list.add(new Float(7));
+		list.add(new Float(-2));
 		
 		assertEquals(5, list.count());
 		assertEquals(0, list.find(a));
@@ -115,17 +115,17 @@ public class TestSimpleList {
 	
 	@Test
 	public void testInsert() {
-		SimpleList<Integer> list = new SimpleList<Integer>();
+		SimpleList<Number> list = new SimpleList<Number>();
 		list.add(new Integer(0));
 		list.add(new Integer(2));
-		list.add(new Integer(-4));
+		list.add(new Double(-4.4));
 		list.add(new Integer(7));
 		list.add(new Integer(-2));
 		
-		list.insert(2,new Integer(666));
+		list.insert(2,new Float(6.66));
 		
-		assertEquals(new Integer(666), list.get(2));
-		assertEquals(new Integer(-4), list.get(3));
+		assertEquals(new Float(6.66), list.get(2));
+		assertEquals(new Double(-4.4), list.get(3));
 	}
 
 }
